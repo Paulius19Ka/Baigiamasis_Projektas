51e7e5e2cf8aae33229b118df04f87f7ed823818
Paulius19Ka <kallabow2@gmail.com>
2025-06-09 10:06:19 +0300

    added a route to get specific user by id

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,3 +1,5 @@
+import { validate as uuidValidate } from 'uuid';
+
 import { connectToDB } from "./helper.js";
 
 const getAllUsers = async (req, res) => {
@@ -17,4 +19,29 @@ const getAllUsers = async (req, res) => {
   };
 };
 
-export { getAllUsers };
+const getUserById = async (req, res) => {
+  const { id } = req.params;
+  const client = await connectToDB();
+
+  if(!uuidValidate(id)){
+    console.error({ error: `[${id}] is not a valid id. The id must be a valid uuid.` });
+    return res.status(400).send({ error: `[${id}] is not a valid id. The id must be a valid uuid.` });
+  };
+
+  let filter = { _id: id };
+  try{
+    const DB_RESPONSE = await client.db('Final_Project').collection('users').findOne(filter);
+    if(!DB_RESPONSE){
+      console.error({ error: `User [id: ${id}] was not found.`});
+      return res.status(404).send({ error: `User [id: ${id}] was not found.`});
+    };
+    res.send(DB_RESPONSE);
+  } catch(err){
+    console.error(err);
+    res.status(500).send({ error: err, message: `Something went wrong with the server.` });
+  } finally{
+    await client.close();
+  };
+};
+
+export { getAllUsers, getUserById };
