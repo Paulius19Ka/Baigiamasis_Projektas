03c7e7f4fb886e69ed929e736a81b6d94ea60003
Paulius19Ka <kallabow2@gmail.com>
2025-06-10 09:51:12 +0300

    fixed height of main, added basic style to footer

--- client/src/components/UI/organisms/Footer.tsx
+++ client/src/components/UI/organisms/Footer.tsx
@@ -1,8 +1,19 @@
+import styled from "styled-components";
+
+const StyledFooter = styled.footer`
+  height: 40px;
+  background-color: var(--background-dark);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 const Footer = () => {
   return (
-    <footer>
+    <StyledFooter>
       <span>Copyright PK 2025</span>
-    </footer>
+    </StyledFooter>
   );
 }
  
--- client/src/components/outlets/MainOutlet.tsx
+++ client/src/components/outlets/MainOutlet.tsx
@@ -1,14 +1,26 @@
 import { Outlet } from "react-router";
+import styled from "styled-components";
 import Header from "../UI/organisms/Header";
 import Footer from "../UI/organisms/Footer";
+
+const StyledMain = styled.main`
+  /* total height - header - footer - (margin top + margin bottom) */
+  min-height: calc(100vh - 40px - 40px - 20px);
+
+  margin: 10px 10px;
+
+  h1, h2, h3, h4, h5, h6{
+    margin: 0;
+  }
+`;
 
 const MainOutlet = () => {
   return (
     <>
       <Header />
-      <main>
+      <StyledMain>
         <Outlet />
-      </main>
+      </StyledMain>
       <Footer />
     </>
   );
