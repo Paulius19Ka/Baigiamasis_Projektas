549785e749df49bd94cbb17d3e18ff4b5bd834bc
Paulius19Ka <kallabow2@gmail.com>
2025-06-13 10:58:37 +0300

    create posts route

--- server/controllers/postsController.js
+++ server/controllers/postsController.js
@@ -1,4 +1,4 @@
-import { validate as uuidValidate } from 'uuid';
+import { validate as uuidValidate, v4 as genID } from 'uuid';
 
 import { connectToDB } from "./helper.js";
 
@@ -44,4 +44,51 @@ const getPostById = async (req, res) => {
   };
 };
 
-export { getAllPosts, getPostById };
+const topics = [ 'Misc', 'General', 'Releases', 'Cllecting', 'Concerts', 'Rock/Blues', 'Pop/Dance', 'Metal/Hard Rock', 'Jazz', 'Classical', 'Electronic', 'Country/Folk', 'Soul/Rap', 'Alternative'];
+
+const createPost = async (req, res) => {
+  const { title, content, topic, userId } = req.body;
+
+  if(!uuidValidate(userId)){
+    console.error({ error: `[${userId}] is not a valid id. The id must be a valid uuid.` });
+    return res.status(400).send({ error: `[${userId}] is not a valid id. The id must be a valid uuid.` });
+  };
+
+  if(!title || !content || !topic || !userId){
+    return res.status(400).send({ error: 'Missing required fields, please enter - title, content, topic, userId.'});
+  };
+
+  if(!topics.includes(topic)){
+    return res.status(400).send({ error: `Incorrect topic. Allowed topics: ${topics.join(', ')}.` });
+  }
+
+  const client = await connectToDB();
+  try{
+    const user = await client.db('Final_Project').collection('users').findOne({ _id: userId });
+    if(!user){
+      return res.status(404).send({ error: 'User not found.' });
+    };
+
+    const newPost = {
+      _id: genID(),
+      postDate: new Date(),
+      postedBy: {
+        username: user.username,
+        userId: user._id
+      },
+      title: title,
+      content: content,
+      topic: topic,
+      score: 0
+    };
+    const DB_RESPONSE = await client.db('Final_Project').collection('posts').insertOne(newPost);
+    res.send(DB_RESPONSE);
+  } catch(err){
+    console.error(err);
+    res.status(500).send({ error: err, message: `Something went wrong with the server.` });
+  } finally{
+    await client.close();
+  };
+};
+
+export { getAllPosts, getPostById, createPost };
--- server/routes/postsRoutes.js
+++ server/routes/postsRoutes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 
-import { getAllPosts, getPostById } from "../controllers/postsController.js";
+import { getAllPosts, getPostById, createPost } from "../controllers/postsController.js";
 
 const router = Router();
 
 router.get('', getAllPosts);
+
 router.get('/:id', getPostById);
+
+router.post('', createPost);
 
 export default router;
