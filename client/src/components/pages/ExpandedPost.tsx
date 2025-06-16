8f2a6314cade5dd31c7d7ab72cf1354781f730f6
Paulius19Ka <kallabow2@gmail.com>
2025-06-16 11:21:10 +0300

    specific post page with some basic info

--- client/src/components/pages/ExpandedPost.tsx
+++ client/src/components/pages/ExpandedPost.tsx
@@ -1,7 +1,35 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+
+import { Post } from "../../types";
+
 const ExpandedPost = () => {
+
+  const { id } = useParams();
+  const [post, setPost] = useState<Post | null>(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:5500/posts/${id}`)
+      .then(res => res.json())
+      .then(data => setPost(data));
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <section>
-      <h2>Post</h2>
+      {
+        post ?
+        <div>
+          <p>Score: {post.score}</p>
+          <p>Posted: {post.postDate ? post.postDate.slice(0, 10): ''}, {post.postDate ? post.postDate.slice(11, 16): ''}</p>
+          <h2>{post.title}</h2>
+          <p>{post.content}</p>
+          <p>Topic: {post.topic}</p>
+          <p>Edited: {post.lastEditDate ? post.lastEditDate.slice(0, 10): ''}, {post.lastEditDate ? post.lastEditDate.slice(11, 16): ''}</p>
+          <p>By: {post.postedBy.username}</p>
+        </div> :
+        <p>Loading...</p>
+      }
     </section>
   );
 }
