db15eb68e40632f0f00aac412e9e8ae3750da7ba
Paulius19Ka <kallabow2@gmail.com>
2025-06-18 11:59:48 +0300

    backend - post saving, deleting functionality

--- server/controllers/savePostsController.js
+++ server/controllers/savePostsController.js
@@ -1,7 +1,12 @@
-import { connectToDB } from "./helper.js";
+import { connectToDB, validateUUID } from "./helper.js";
 
+// SAVE POST
 const savePost = async (req, res) => {
   const { userId, postId } = req.params;
+
+  validateUUID(userId, res);
+  validateUUID(postId, res);
+
   const client = await connectToDB();
   try{
     let filter = { _id: userId };
@@ -21,4 +26,53 @@ const savePost = async (req, res) => {
   };
 };
 
-export { savePost };
+// GET ALL SAVED POSTS
+const getSavedPosts = async (req, res) => {
+  const { userId } = req.params;
+
+  validateUUID(userId, res);
+
+  const client = await connectToDB();
+  try{
+    let filter = { _id: userId };
+    const DB_Response = await client.db('Final_Project').collection('users').findOne(filter);
+    if(!DB_Response){
+      return res.status(404).send({ error: `User with ID: ${userId} was not found.`});
+    };
+
+    res.send({ savedPosts: DB_Response.savedPosts });
+  } catch(err){
+    console.error(err);
+    res.status(500).send({ error: err, message: `Something went wrong with server.` });
+  } finally{
+    await client.close();
+  };
+};
+
+// REMOVE SAVED POST
+const removeSavedPost = async (req, res) => {
+  const { userId, postId } = req.params;
+
+  validateUUID(userId, res);
+  validateUUID(postId, res);
+
+  const client = await connectToDB();
+  try{
+    let filter = { _id: userId };
+    // removes all isntances of the value from array
+    let update = { $pull: { savedPosts: postId } };
+    const DB_Response = await client.db('Final_Project').collection('users').updateOne(filter, update);
+    if(DB_Response.matchedCount === 0){
+      return res.status(404).send({ error: `User with ID: ${userId} was not found.`});
+    };
+
+    res.send({ success: `Post removed from saved posts successfully.` });
+  } catch(err){
+    console.error(err);
+    res.status(500).send({ error: err, message: `Something went wrong with server.` });
+  } finally{
+    await client.close();
+  };
+};
+
+export { savePost, getSavedPosts, removeSavedPost };
--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { autoLogin, editUser, getId, login, refreshLogin, register } from "../controllers/usersController.js";
 import { verifyAdmin, verifyJWT } from "../middleware/auth.js";
-import { savePost } from "../controllers/savePostsController.js";
+import { savePost, getSavedPosts, removeSavedPost } from "../controllers/savePostsController.js";
 
 const router = Router();
 
@@ -21,6 +21,11 @@ router.get('/getId', verifyJWT, getId);
 
 router.patch('/:id', editUser);
 
-router.post('/:id/savePost/:id', savePost);
+// SAVING OF POSTS ROUTES
+router.post('/:userId/savePost/:postId', savePost);
+
+router.get('/:userId/getSavedPosts', getSavedPosts);
+
+router.delete('/:userId/deleteSavedPost/:postId', removeSavedPost);
 
 export default router;
