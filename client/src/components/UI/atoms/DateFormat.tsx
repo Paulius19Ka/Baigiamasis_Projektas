04d2e0485ef4412889652ff71f5b758f7781f137
Paulius19Ka <kallabow2@gmail.com>
2025-06-24 22:09:22 +0300

    updated the date format component to show today and yesterday

--- client/src/components/UI/atoms/DateFormat.tsx
+++ client/src/components/UI/atoms/DateFormat.tsx
@@ -4,15 +4,35 @@ type Props = {
 
 const DateFormat = ({ date }: Props) => {
 
-  const createDate = new Date(date);
-  const formattedDate = createDate.toLocaleString(undefined, {
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
+  const inputDate = new Date(date);
+  const currentDate = new Date();
+
+  const sameDay = inputDate.toDateString() === currentDate.toDateString();
+
+  const yesterdaysDate = new Date();
+  yesterdaysDate.setDate(currentDate.getDate() - 1)
+  const isYesterday = inputDate.toDateString() === yesterdaysDate.toDateString();
+
+  const time = inputDate.toLocaleTimeString(undefined, {
     hour: '2-digit',
     minute: '2-digit',
     hour12: false
   });
+
+  let formattedDate = '';
+
+  if(sameDay){
+    formattedDate = `Today at ${time}`;
+  } else if(isYesterday){
+    formattedDate = `Yesterday at ${time}`;
+  } else{
+    const date = inputDate.toLocaleDateString(undefined, {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric',
+    }); 
+    formattedDate = `${date} at ${time}`;
+  };
 
   return (
     <>{formattedDate}</>
